be854fba5e749a40c7ebfe14e9026a2ad437b6d8
Mateus Luiz <mateuslfreitasb@gmail>
2017-02-18 22:13:16 -0200

    Setting up devise for login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+User.create([
+  {
+      email: 'user@user.com',
+      password: '123456'
+  }
+  ])
+
+
+p "#{User.count} users created"
