00b646e9bd49d52e0851d84d7b82af501b3e20e4
Mateus Luiz <mateuslfreitasb@gmail>
2017-03-08 18:53:43 -0300

    Docx generator maintenance

--- app/controllers/document_generator_controller.rb
+++ app/controllers/document_generator_controller.rb
@@ -8,16 +8,16 @@ class DocumentGeneratorController < ApplicationController
       f.docx do
         doc = DocxReplace::Doc.new("#{Rails.root}/lib/docx_templates/pauta.docx", "#{Rails.root}/tmp")
 
-        doc.replace("{dataAudiencia}", @process.court_hearing_date)
-        doc.replace("{horaAudiencia}", @process.court_hearing_time)
-        doc.replace("{valorAnterior}", "")
-        doc.replace("{anoProcessso}", @process.year_process)
-        doc.replace("{numeroProcesso}", @process.nu_process)
-        doc.replace("{acao}", @process.action)
-        doc.replace("{nomeAutor}", @process.prosecutor_name)
-        doc.replace("{advogadoAutor}", @process.lawyer_author)
-        doc.replace("{nomeReu}", @process.defendent_name)
-        doc.replace("{advogadoReu}", @process.lawyer_defendent)
+        doc.replace("dataAudiencia", @process.court_hearing_date)
+        doc.replace("$horaAudiencia$", @process.court_hearing_time)
+        doc.replace("valorAnterior", "")
+        doc.replace("anoProcesso", @process.year_process)
+        doc.replace("numeroProcesso", @process.nu_process)
+        doc.replace("acao", @process.action)
+        doc.replace("nomeAutor", @process.prosecutor_name)
+        doc.replace("advogadoAutor", @process.lawyer_author)
+        doc.replace("nomeReu", @process.defendent_name)
+        doc.replace("advogadoReu", @process.lawyer_defendent)
 
         tmp_file = Tempfile.new('temp', "#{Rails.root}/tmp")
 
@@ -34,11 +34,11 @@ class DocumentGeneratorController < ApplicationController
       f.docx do
         doc = DocxReplace::Doc.new("#{Rails.root}/lib/docx_templates/termo.docx", "#{Rails.root}/tmp")
 
-        doc.replace("{numeroProcesso}", @process.nu_process)
-        doc.replace("{anoProcesso}", @process.year_process)
-        doc.replace("{acao}", @process.action)
-        doc.replace("{nomeAutor}", @process.prosecutor_name)
-        doc.replace("{nomeReu}", @process.defendent_name)
+        doc.replace("numeroProcesso", @process.nu_process)
+        doc.replace("anoProcesso", @process.year_process)
+        doc.replace("acao", @process.action)
+        doc.replace("nomeAutor", @process.prosecutor_name)
+        doc.replace("nomeReu", @process.defendent_name)
 
         tmp_file = Tempfile.new('temp', "#{Rails.root}/tmp")
 
