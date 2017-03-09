3eaeb0baae2bbdb071f8782110b094037c51732a
Mateus Luiz <mateuslfreitasb@gmail>
2017-03-09 17:49:17 -0300

    Removing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.delete_all
-User.create([
-  {
-      email: 'user@user.com',
-      password: '123456'
-  }
-  ]);
-
-p "#{User.count} users created"
-
-JudiciaryProcess.delete_all
-JudiciaryProcess.create([
-  {
-    nu_process: '1'
-  },
-  {
-    nu_process: '2'
-  },
-  {
-    nu_process: '3'
-  }
-  ])
-
-p "#{JudiciaryProcess.count} processes count"
